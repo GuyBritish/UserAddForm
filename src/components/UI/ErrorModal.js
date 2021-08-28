8293c2b03f79b394b3ac0dc7bb0e0fccbd9c6e9a
Max <80323354+GuyBritish@users.noreply.github.com>
2021-08-28 16:56:39 -0500

    Add dynamic error messages

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -7,19 +7,22 @@ import styles from "./ErrorModal.module.css";
 
 function ErrorModal(props) {
 	return (
-		<Card className={styles.modal}>
-			<header className={styles.header}>
-				<h2> {props.title} </h2>
-			</header>
+		<div>
+			<div className={styles.backdrop} />
+			<Card className={styles.modal}>
+				<header className={styles.header}>
+					<h2> {props.title} </h2>
+				</header>
 
-			<div className={styles.content}>
-				<p> {props.message} </p>
-			</div>
+				<div className={styles.content}>
+					<p> {props.message} </p>
+				</div>
 
-			<footer className={styles.action}>
-				<Button> Okay </Button>
-			</footer>
-		</Card>
+				<footer className={styles.actions}>
+					<Button> Okay </Button>
+				</footer>
+			</Card>
+		</div>
 	);
 }
 
