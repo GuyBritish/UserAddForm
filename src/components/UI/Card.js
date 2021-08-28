21c837fc6e54488ae4993736d675ff56b4198189
Max <80323354+GuyBritish@users.noreply.github.com>
2021-08-28 13:42:31 -0500

    Fix typo

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -3,7 +3,7 @@ import React from "react";
 import styles from "./Card.module.css";
 
 function Card(props) {
-	return <div className={styles.card}> {props.chidren} </div>;
+	return <div className={styles.card}>{props.children}</div>;
 }
 
 export default Card;
