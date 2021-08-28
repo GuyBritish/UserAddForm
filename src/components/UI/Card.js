7cacc1b117475d25b8951425b3563e63891f7d68
Max <80323354+GuyBritish@users.noreply.github.com>
2021-08-28 15:36:32 -0500

    Add class from parent

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -3,7 +3,7 @@ import React from "react";
 import styles from "./Card.module.css";
 
 function Card(props) {
-	return <div className={styles.card}>{props.children}</div>;
+	return <div className={`${props.className} ${styles.card}`}>{props.children}</div>;
 }
 
 export default Card;
